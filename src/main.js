a72ee721455bb6a725a0edb6b14713b96bb3a9c9
Garrison McMullen <mcmullengn@gmail.com>
2021-11-16 21:10:42 -0500

    feat: add custom 404 page

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ import SearchBar from '~/components/SearchBar.vue'
 import Citations from '~/components/Citations.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faTimes, faRandom } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faTimes, faRandom, faHiking } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import moment from 'moment';
 
 config.autoAddCss = false;
-library.add(faSearch, faTimes, faRandom);
+library.add(faSearch, faTimes, faRandom, faHiking);
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
---
+++ src/pages/404.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout>
+    <div class="post-container">
+      <div class="content-container">
+        <font-awesome :icon="['fa', 'hiking']" style="font-size:100px" /> 
+        <span> You've wandered off. We can't find what you're looking for. </span>
+        <g-link to="/">Keep searching for an answer.</g-link> 
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
+
+<style scoped>
+h1 { 
+  margin: 0;
+  border-bottom: 1px solid var(--main-border-color);
+}
+
+.content-container { 
+  text-align: center;
+  padding: 10px 0;
+  border-bottom: 1px solid var(--main-border-color);
+}
+
+.content-container * { 
+  display: block;
+  width: 100%;
+  margin-bottom: 15px;
+}
+
+.date-container { 
+  margin-top: 15px;
+}
+</style>
